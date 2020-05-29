43dbe1c2f09774b9073745ee2433c825cfa8c936
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-05-29 00:31:14 -0300

    second
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- Makefile
+++ Makefile
@@ -1,4 +1,19 @@
+setup:
+	brew install qemu rustup
+	rustup-init
+	rustup override add nightly
+	cargo install bootimage
+	rustup component add llvm-tools-preview
+
 build:
-	# the "none" target triple is to say we don't want an underlying OS.
-	# this prevents rust trying to link the C runtime
-	cargo build --target thumbv7em-none-eabihf
+	cargo xbuild
+
+build-target:
+	cargo xbuild --target x86_64-beckeros.json
+
+img:
+	cargo bootimage
+
+run:
+	# qemu-system-x86_64 -drive format=raw,file=target/x86_64-beckeros/debug/bootimage-beckeros.bin
+	cargo xrun
--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,22 @@ fn panic(_info: &PanicInfo) -> ! {
 	loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle] // makes the function be really called _start
 pub extern "C" fn _start() -> ! {
 	// this is the entry point, as the linker will look for a func called
 	// `_start` by default
+
+	// 0xb8000 is the VGA buffer addr, this converts it into a raw pointer
+	let vga_buffer = 0xb8000 as *mut u8;
+
+	for (i, &byte) in HELLO.iter().enumerate() {
+		unsafe {
+			*vga_buffer.offset(i as isize * 2) = byte;
+			*vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+		}
+	}
+
 	loop {}
 }
