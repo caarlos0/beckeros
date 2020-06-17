a69aaaedce0c27632014d276ef710f33c7c73e4c
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-06-17 00:33:09 -0300

    wip
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,10 @@ use core::panic::PanicInfo;
 pub mod serial;
 pub mod vga_buffer;
 
+func init() {
+	interrupts::init_idt();
+}
+
 pub fn test_runner(tests: &[&dyn Fn()]) {
 	serial_println!("Running {} tests...", tests.len());
 	for test in tests {
